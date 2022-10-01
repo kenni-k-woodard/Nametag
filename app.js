b5e0732beb9a8021f10edf08511ccb1fe4784724
Kenni Woodard <kennikristine@gmail.com>
2022-10-01 16:37:28 -0700

    button works now

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // grab DOM elements
 
 // set event listeners
+console.log('hey there');
+const nameDisplay = document.getElementById('name-display');
+const updateBtn = document.getElementById('updateBtn');
+const nameInput = document.getElementById('nameInput');
+
+updateBtn.addEventListener('click', () => {
+    const nameInput = document.getElementById('nameInput');
+
+    nameDisplay.textContent = nameInput.value;
+})
+
+
