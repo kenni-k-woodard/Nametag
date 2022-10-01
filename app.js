a339c5613d5c5fc20b1cb2c153efff5a4d433a51
Kenni Woodard <kennikristine@gmail.com>
2022-10-01 16:52:58 -0700

    fixing errors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // grab DOM elements
 
 // set event listeners
-console.log('hey there');
+
 const nameDisplay = document.getElementById('name-display');
 const updateBtn = document.getElementById('updateBtn');
 const nameInput = document.getElementById('nameInput');
@@ -10,6 +10,6 @@ updateBtn.addEventListener('click', () => {
     const nameInput = document.getElementById('nameInput');
 
     nameDisplay.textContent = nameInput.value;
-})
+});
 
 
